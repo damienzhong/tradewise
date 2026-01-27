d3bfe809013a550e0dfc2681dbeafeed0864f535
damienzhong <damienzhong@foxmail.com>
2026-01-28 00:08:08 +0800

    Create TwPasswordEncoder.java

---
+++ src/main/java/com/damien/tradewise/common/service/TwPasswordEncoder.java
@@ -0,0 +1,27 @@
+package com.damien.tradewise.common.service;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+/**
+ * 密码加密工具类
+ */
+@Component
+public class TwPasswordEncoder {
+    
+    private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+    
+    /**
+     * 加密密码
+     */
+    public String encode(String rawPassword) {
+        return encoder.encode(rawPassword);
+    }
+    
+    /**
+     * 验证密码
+     */
+    public boolean matches(String rawPassword, String encodedPassword) {
+        return encoder.matches(rawPassword, encodedPassword);
+    }
+}
