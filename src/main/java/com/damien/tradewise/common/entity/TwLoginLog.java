e7b0126c9780473a1a6955790a2bb76b7bdfce04
damienzhong <damienzhong@foxmail.com>
2026-01-28 00:05:11 +0800

    Create TwLoginLog.java

---
+++ src/main/java/com/damien/tradewise/common/entity/TwLoginLog.java
@@ -0,0 +1,87 @@
+package com.damien.tradewise.common.entity;
+
+import java.time.LocalDateTime;
+
+public class TwLoginLog {
+    private Long id;
+    private String userType;
+    private Long userId;
+    private String username;
+    private String loginIp;
+    private String userAgent;
+    private String loginStatus;
+    private String failReason;
+    private LocalDateTime loginTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getLoginIp() {
+        return loginIp;
+    }
+
+    public void setLoginIp(String loginIp) {
+        this.loginIp = loginIp;
+    }
+
+    public String getUserAgent() {
+        return userAgent;
+    }
+
+    public void setUserAgent(String userAgent) {
+        this.userAgent = userAgent;
+    }
+
+    public String getLoginStatus() {
+        return loginStatus;
+    }
+
+    public void setLoginStatus(String loginStatus) {
+        this.loginStatus = loginStatus;
+    }
+
+    public String getFailReason() {
+        return failReason;
+    }
+
+    public void setFailReason(String failReason) {
+        this.failReason = failReason;
+    }
+
+    public LocalDateTime getLoginTime() {
+        return loginTime;
+    }
+
+    public void setLoginTime(LocalDateTime loginTime) {
+        this.loginTime = loginTime;
+    }
+}
